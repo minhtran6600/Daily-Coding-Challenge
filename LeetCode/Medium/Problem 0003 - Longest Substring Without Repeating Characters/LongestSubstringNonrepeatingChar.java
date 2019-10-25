288ce0e289f616aa207e5a2054f5a605f40e67da
Minh Tran <tnnguyenminhdn@gmail.com>
2019-10-25 05:30:30 -0500

    Problem 3

---
+++ LeetCode/Medium/Problem 0003 - Longest Substring Without Repeating Characters/LongestSubstringNonrepeatingChar.java
@@ -0,0 +1,38 @@
+/*
+ * Given a string, find the length of the longest substring without repeating characters.
+ */
+
+public class LongestSubstringNonrepeatingChar {
+    public int lengthOfLongestSubstring(String s) {
+        int len = s.length();
+        int[] repeatIndex = new int[128];
+
+        int maxLen = 0;
+        int countLen = 0;
+        int head = 1;
+        for (int i = 0; i < len; i++)
+        {
+            int index = repeatIndex[s.charAt(i)];
+            if (head > index)
+                countLen++;
+            else
+            {
+                maxLen = Math.max(countLen, maxLen);
+                countLen = countLen - (index - head);
+                head = index + 1;
+            }
+            repeatIndex[s.charAt(i)] = i + 1;
+        }
+        return Math.max(countLen, maxLen);
+    }
+
+    public void test(String s)
+    {
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+
+    public static void main(String[] args) {
+        LongestSubstringNonrepeatingChar lsnc = new LongestSubstringNonrepeatingChar();
+        lsnc.test("pwwkew");
+    }
+}
